57e2cb87646178c1b99c384da79cf472e3d24715
owner <owner@localhost>
2026-04-20 12:06:03 +0900

    クッキー

--- src/src/routes/auth.ts
+++ src/src/routes/auth.ts
@@ -1,47 +1,49 @@
-import { Hono } from "hono"
-import { setCookie, deleteCookie } from "hono/cookie"
-import { encryptSession } from "../services/app/session"
-import { auth } from "../services/app/middleware"
-import type { AppVariables } from "../types/types"
+import { Hono } from 'hono';
+import { setCookie, deleteCookie } from 'hono/cookie';
+import { encryptSession } from '../services/app/session';
+import { auth } from '../services/app/middleware';
+import type { AppVariables } from '../types/types';
 
-export const authRoute = new Hono<{ Bindings: Env, Variables: AppVariables }>();
+export const authRoute = new Hono<{ Bindings: Env; Variables: AppVariables }>();
 
 // ログイン
-authRoute.post("/login", async (c) => {
-  const body = await c.req.json()
+authRoute.post('/login', async (c) => {
+  const body = await c.req.json();
 
-  if (
-    body.email === "test@localhost.com" &&
-    body.password === "testtest"
-  ) {
+  if (body.email === 'test@localhost.com' && body.password === 'testtest') {
     const token = await encryptSession({
       id: 1,
       email: body.email,
-      exp: Date.now() + 1000 * 60 * 60 * 24
-    })
+      exp: Date.now() + 1000 * 60 * 60 * 24,
+    });
 
-    console.log({token})
+    console.log({ token });
 
-    setCookie(c, "session", token, {
+    setCookie(c, 'session', token, {
       httpOnly: true,
       secure: true,
-      path: "/",
-      sameSite: "None"
-    })
+      path: '/',
+      sameSite: 'None',
+    });
 
-    return c.json({ ok: true })
+    return c.json({ ok: true });
   }
 
-  return c.json({ error: "Login failed" }, 401)
-})
+  return c.json({ error: 'Login failed' }, 401);
+});
 
 // 認証ユーザー
-authRoute.get("/me", auth, (c) => {
-  return c.json(c.get("user"))
-})
+authRoute.get('/me', auth, (c) => {
+  return c.json(c.get('user'));
+});
 
 // ログアウト
-authRoute.post("/logout", (c) => {
-  deleteCookie(c, "session")
-  return c.json({ ok: true })
-})
+authRoute.post('/logout', (c) => {
+  deleteCookie(c, 'session', {
+    httpOnly: true,
+    secure: true,
+    path: '/',
+    sameSite: 'None',
+  });
+  return c.json({ ok: true });
+});
