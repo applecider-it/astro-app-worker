311075b352142b151c45e87aab0afb6633c8eb8c
owner <owner@localhost>
2026-04-21 12:19:49 +0900

    ツイート

--- src/src/services/tweet/list.ts
+++ src/src/services/tweet/list.ts
@@ -3,9 +3,23 @@ import type { AppHonoType } from '../../types/types';
 
 /** ツイート一覧を返す */
 export async function getTweets(c: Context<AppHonoType>) {
-  const { results } = await c.env.DB.prepare(
-    'SELECT * FROM user_tweets ORDER BY id DESC LIMIT 10',
-  ).all();
+  const { results } = await c.env.DB.prepare(`
+    SELECT
+      user_tweets.id,
+      user_tweets.content,
+      user_tweets.created_at,
+
+      users.id   AS user_id,
+      users.name AS user_name,
+      users.email AS user_email
+
+    FROM user_tweets
+    INNER JOIN users
+      ON users.id = user_tweets.user_id
+
+    ORDER BY user_tweets.id DESC
+    LIMIT 10
+  `).all();
 
   return results;
 }
