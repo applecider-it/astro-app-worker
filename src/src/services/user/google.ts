5f6fc2c3724712d2ce69c40a0beb718d9f6eecc0
owner <owner@localhost>
2026-04-21 18:22:54 +0900

    google認証

--- src/src/services/user/google.ts
+++ src/src/services/user/google.ts
@@ -24,7 +24,7 @@ export function setupGoogleAuth(c: Context<AppHonoType>) {
 
   const cookieOptions = {
     httpOnly: true,
-    secure: false, // local開発ならfalse
+    secure: true,
     path: '/',
     maxAge: 600,
   };
